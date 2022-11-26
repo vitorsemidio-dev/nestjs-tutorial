41c21688b321b858d9956e2ce32ea7c5f094d851
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-11-26 16:43:14 -0300

    feat: search customer by id

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 import { CustomersService } from 'src/customers/services/customers/customers.service';
 
 @Controller('customers')
@@ -8,5 +18,29 @@ export class CustomersController {
   @Get('')
   getCustomers() {
     return this.customersService.findAllCustomers();
+  }
+
+  @Get(':id')
+  getCustomerById(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const customer = this.customersService.findCustomerById(id);
+    if (customer) {
+      res.send(customer);
+    } else {
+      res.status(400).send({ msg: 'Customer not found!' });
+    }
+  }
+
+  @Get('/search/:id')
+  searchCustomerById(@Param('id', ParseIntPipe) id: number) {
+    const customer = this.customersService.findCustomerById(id);
+    if (customer) {
+      return customer;
+    } else {
+      throw new HttpException('Customer not found', HttpStatus.BAD_REQUEST);
+    }
   }
 }
--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -2,18 +2,24 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
+  private users = [
+    {
+      id: 1,
+      email: 'po@tuwzacwi.ru',
+      createdAt: new Date(),
+    },
+    {
+      id: 2,
+      email: 'ipi@febzoziz.dm',
+      createdAt: new Date(),
+    },
+  ];
+
   findAllCustomers() {
-    return [
-      {
-        id: 1,
-        email: 'po@tuwzacwi.ru',
-        createdAt: new Date(),
-      },
-      {
-        id: 2,
-        email: 'ipi@febzoziz.dm',
-        createdAt: new Date(),
-      },
-    ];
+    return this.users;
+  }
+
+  findCustomerById(id: number) {
+    return this.users.find((user) => user.id === id);
   }
 }
