a8bfe71b391a2429cc5375bd9ce30730a56871b1
Vitor Emídio <vitorsemidio.96@gmail.com>
2022-11-27 13:09:57 -0300

    test: add tests to `payments.controller`

--- src/payments/controllers/payments/payments.controller.spec.ts
+++ src/payments/controllers/payments/payments.controller.spec.ts
@@ -1,5 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Request, Response } from 'express';
 import { PaymentsController } from './payments.controller';
+
+const makeMock = () => {
+  const requestMock = {
+    query: {},
+  } as unknown as Request;
+  const sendMockFn = {
+    send: jest.fn((x) => x),
+  };
+  const responseMock = {
+    status: jest.fn((x) => sendMockFn),
+    send: jest.fn((x) => x),
+  } as unknown as Response;
+
+  return {
+    requestMock,
+    responseMock,
+    sendMockFn,
+  };
+};
 
 describe('PaymentsController', () => {
   let controller: PaymentsController;
@@ -14,5 +34,50 @@ describe('PaymentsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getPayments', () => {
+    it('should return a status of 400 when query params are not provided', () => {
+      const { requestMock, responseMock, sendMockFn } = makeMock();
+      controller.getPayments(requestMock, responseMock);
+      expect(responseMock.status).toHaveBeenCalledWith(400);
+      expect(sendMockFn.send).toHaveBeenCalledWith({
+        msg: 'Missing count or page query parameter',
+      });
+    });
+    it('should return a status of 400 when page is not provided', () => {
+      const { requestMock, responseMock, sendMockFn } = makeMock();
+      requestMock.query = {
+        count: '1',
+      };
+
+      controller.getPayments(requestMock, responseMock);
+      expect(responseMock.status).toHaveBeenCalledWith(400);
+      expect(sendMockFn.send).toHaveBeenCalledWith({
+        msg: 'Missing count or page query parameter',
+      });
+    });
+
+    it('should return a status of 400 when count not provided', () => {
+      const { requestMock, responseMock, sendMockFn } = makeMock();
+      requestMock.query = {
+        page: '1',
+      };
+      controller.getPayments(requestMock, responseMock);
+      expect(responseMock.status).toHaveBeenCalledWith(400);
+      expect(sendMockFn.send).toHaveBeenCalledWith({
+        msg: 'Missing count or page query parameter',
+      });
+    });
+
+    it('should return a status of 200 when query params are provided', () => {
+      const { requestMock, responseMock } = makeMock();
+      requestMock.query = {
+        count: '1',
+        page: '1',
+      };
+      controller.getPayments(requestMock, responseMock);
+      expect(responseMock.send).toHaveBeenCalledWith(200);
+    });
   });
 });
--- src/payments/controllers/payments/payments.controller.ts
+++ src/payments/controllers/payments/payments.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('payments')
-export class PaymentsController {}
+export class PaymentsController {
+  @Get('')
+  getPayments(@Req() req: Request, @Res() res: Response) {
+    const { count, page } = req.query;
+    if (!count || !page) {
+      res.status(400).send({ msg: 'Missing count or page query parameter' });
+    }
+    res.send(200);
+  }
+}
