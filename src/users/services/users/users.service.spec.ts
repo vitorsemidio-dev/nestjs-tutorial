bc722fc596591a59d0318265c3d6434735a7ad55
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-11-28 02:31:02 -0300

    refactor: move mock implementation to beforeEach

--- src/users/services/users/users.service.spec.ts
+++ src/users/services/users/users.service.spec.ts
@@ -1,10 +1,11 @@
-import { UserEmailAlreadyExits } from './../../exceptions/UserEmailAlreadyExists.exception';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { randomInt } from 'crypto';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { User } from 'src/users/entities/User.entity';
+import { UserEmailAlreadyExits } from 'src/users/exceptions/UserEmailAlreadyExists.exception';
+import { HashService } from 'src/users/services/hash/hash.service';
 import { Repository } from 'typeorm';
-import { HashService } from '../hash/hash.service';
-import { User } from './../../entities/User.entity';
 import { UsersService } from './users.service';
 
 const USER_REPOSITORY_TOKEN = getRepositoryToken(User);
@@ -12,22 +13,50 @@ const HASH_SERVICE_TOKEN = 'HASH_SERVICE';
 
 const hashFnMock = (payload: string) => `${payload}hashed`;
 
+const makeSut = () => {
+  const user1: User = {
+    id: 1,
+    email: 'faitmo@lilel.it',
+    password: '12345678hashed',
+    username: 'faitmo',
+  };
+  const user2 = {
+    id: 2,
+    email: 'datnuico@suvvejone.ne',
+    password: '12345678hashed',
+    username: 'datnuico',
+  };
+  return {
+    user1,
+    user2,
+  };
+};
+
 describe('UsersService', () => {
   let usersService: UsersService;
   let usersRepository: Repository<User>;
   let hashService: HashService;
 
   beforeEach(async () => {
+    const { user1, user2 } = makeSut();
+    const userDb: User[] = [user1, user2];
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
         {
           provide: USER_REPOSITORY_TOKEN,
           useValue: {
-            create: jest.fn((x) => ({ ...x, id: 1 })),
-            find: jest.fn(() => []),
-            findOne: jest.fn((x) => x),
-            save: jest.fn((x) => x),
+            create: jest.fn((x) => ({ ...x, id: randomInt(1000) })),
+            find: jest.fn(() => userDb),
+            findOne: jest.fn((x) => {
+              const key = Object.keys(x.where)[0];
+              const value = x.where[key];
+              return userDb.find((user) => user[key] === value) || null;
+            }),
+            save: jest.fn((x) => {
+              userDb.push(x);
+              return x;
+            }),
           },
         },
         {
@@ -70,9 +99,6 @@ describe('UsersService', () => {
     };
     it('should create a new user with encoded password', async () => {
       const { input } = makeInputDto();
-      jest
-        .spyOn(usersService, 'findUserByEmail')
-        .mockImplementationOnce(() => null);
       await usersService.createUser(input);
       expect(usersRepository.create).toHaveBeenCalledWith({
         ...input,
@@ -83,13 +109,8 @@ describe('UsersService', () => {
     it('should call the methods with correct values', async () => {
       const { input } = makeInputDto();
       const passwordHashed = hashFnMock(input.password);
-      jest.spyOn(usersService, 'findUserByEmail').mockResolvedValueOnce(null);
-      jest.spyOn(usersRepository, 'create').mockReturnValueOnce({
-        ...input,
-        password: passwordHashed,
-        id: 1,
-      });
-      await usersService.createUser(input);
+      jest.spyOn(usersService, 'findUserByEmail');
+      const output = await usersService.createUser(input);
       expect(usersService.findUserByEmail).toHaveBeenCalledWith(input.email);
       expect(hashService.hash).toHaveBeenCalledWith(input.password);
       expect(usersRepository.create).toHaveBeenCalledWith({
@@ -99,37 +120,24 @@ describe('UsersService', () => {
       expect(usersRepository.save).toHaveBeenCalledWith({
         ...input,
         password: passwordHashed,
-        id: 1,
+        id: output.id,
       });
     });
 
     it('should save a new user with id generated', async () => {
       const { input } = makeInputDto();
       const passwordHashed = hashFnMock(input.password);
-      jest
-        .spyOn(usersService, 'findUserByEmail')
-        .mockImplementationOnce(() => null);
-      jest.spyOn(usersRepository, 'create').mockImplementationOnce(() => ({
-        ...input,
-        password: passwordHashed,
-        id: 1,
-      }));
-      await usersService.createUser(input);
+      const output = await usersService.createUser(input);
       expect(usersRepository.save).toHaveBeenCalledWith({
         ...input,
         password: passwordHashed,
-        id: 1,
+        id: output.id,
       });
+      expect(output).toHaveProperty('id');
     });
 
     it('should not be able to create a user with a e-mail already in use', async () => {
-      const { input } = makeInputDto();
-      jest.spyOn(usersService, 'findUserByEmail').mockResolvedValueOnce({
-        id: 10,
-        email: input.email,
-        password: 'any_password_hashed',
-        username: 'any_username',
-      });
+      const { user1: input } = makeSut();
       const output = usersService.createUser(input);
       await expect(output).rejects.toThrow(UserEmailAlreadyExits);
     });
@@ -141,29 +149,18 @@ describe('UsersService', () => {
       return { email };
     };
     it('should return user if email exists', async () => {
-      const { email } = makeInputEmail();
-      jest.spyOn(usersRepository, 'findOne').mockResolvedValueOnce({
-        id: 1,
-        email: email,
-        username: 'dimluip',
-        password: '12345678hashed',
-      });
+      const { user1 } = makeSut();
+      const email = user1.email;
       const output = await usersService.findUserByEmail(email);
       expect(output).toBeDefined();
       expect(usersRepository.findOne).toHaveBeenCalledWith({
         where: { email },
       });
-      expect(output).toMatchObject({
-        id: 1,
-        email,
-        username: 'dimluip',
-        password: '12345678hashed',
-      });
+      expect(output).toMatchObject(user1);
     });
 
     it('should return "null" if email not exists', async () => {
       const { email } = makeInputEmail();
-      jest.spyOn(usersRepository, 'findOne').mockResolvedValueOnce(null);
       const output = await usersService.findUserByEmail(email);
       expect(output).toBeNull();
       expect(usersRepository.findOne).toHaveBeenCalledWith({
@@ -173,7 +170,6 @@ describe('UsersService', () => {
 
     it('should call the methods with correct values', async () => {
       const { email } = makeInputEmail();
-      jest.spyOn(usersRepository, 'findOne').mockResolvedValueOnce(null);
       await usersService.findUserByEmail(email);
       expect(usersRepository.findOne).toHaveBeenCalledWith({
         where: { email },
@@ -187,29 +183,18 @@ describe('UsersService', () => {
       return { username };
     };
     it('should return user if username exists', async () => {
-      const { username: username } = makeInputUsername();
-      jest.spyOn(usersRepository, 'findOne').mockResolvedValueOnce({
-        id: 1,
-        email: 'dimluip@mowan.nu',
-        username,
-        password: '12345678hashed',
-      });
+      const { user2 } = makeSut();
+      const { username } = user2;
       const output = await usersService.findUserByUsername(username);
       expect(output).toBeDefined();
       expect(usersRepository.findOne).toHaveBeenCalledWith({
         where: { username },
       });
-      expect(output).toMatchObject({
-        id: 1,
-        email: 'dimluip@mowan.nu',
-        username,
-        password: '12345678hashed',
-      });
+      expect(output).toMatchObject(user2);
     });
 
-    it('should return "null" if email not exists', async () => {
+    it('should return "null" if username not exists', async () => {
       const { username } = makeInputUsername();
-      jest.spyOn(usersRepository, 'findOne').mockResolvedValueOnce(null);
       const output = await usersService.findUserByUsername(username);
       expect(output).toBeNull();
       expect(usersRepository.findOne).toHaveBeenCalledWith({
@@ -219,7 +204,6 @@ describe('UsersService', () => {
 
     it('should call the methods with correct values', async () => {
       const { username } = makeInputUsername();
-      jest.spyOn(usersRepository, 'findOne').mockResolvedValueOnce(null);
       await usersService.findUserByUsername(username);
       expect(usersRepository.findOne).toHaveBeenCalledWith({
         where: { username },
