42b7e5a7a74158a223799cc0e08dcefe269d932a
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-10 19:37:02 -0300

    chore: setup test e2e

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
+import { CommonModule } from 'src/common/common.module';
 import { CustomersModule } from 'src/customers/customers.module';
 import { DatabaseModule } from 'src/database/database.module';
+import { PaymentsModule } from 'src/payments/payments.module';
 import { UsersModule } from 'src/users/users.module';
-import { PaymentsModule } from './payments/payments.module';
 
 @Module({
   imports: [
+    CommonModule,
     AuthModule,
     CustomersModule,
     DatabaseModule,
